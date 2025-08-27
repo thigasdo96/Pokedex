d1667206e2783514893c3956fd6e65b9f1f345d4
thigasdo96 <primothiago8@gmail.com>
2025-08-26 22:35:18 -0300

    POkedexIniciado

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,57 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pokedex.Models;
+[Table("Pokemon")]
+public class Pokemon
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public uint Numero { get; set; }
+
+
+    [Required(ErrorMessage = "Por favor, informe a região")]
+
+    public uint RegiaoId { get; set; }
+    [ForeignKey("RegiaoId")]
+
+    public Regiao Regiao { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o genero")]
+
+    
+    public uint GerneroId { get; set; }
+    [ForeignKey("GeneroId")]
+
+    public Genero Genero { get; set; }
+
+    [StringLength(30)]
+     [Required(ErrorMessage = "Por favor, informe o nome")]
+
+     public string Nome { get; set; }
+
+     [StringLength(1000)]
+
+     public string Descricao { get; set; }
+
+    [Column(TypeName ="double(5,2)")]
+    [Required(ErrorMessage = "Por favor, informe a Altura")]
+
+     public double Altura { get; set; }
+
+ [Column(TypeName ="double(7,3)")]
+    [Required(ErrorMessage = "Por favor, informe o Peso")]
+
+     public double Peso { get; set; }
+
+     [StringLength(200)]
+
+    public string Imagem { get; set; }
+
+    [StringLength(400)]
+
+    public string Animacao { get; set; }
+
+    public ICollection<PokemonTipo> Tipos { get; set; }
+}
